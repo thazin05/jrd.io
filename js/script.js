7175ee8c4550bca52d2f4caa60c3eec81eccd66b
thazin05 <ttthazinthazin@gmail.com>
2017-09-26 14:35:03 +0800

    add files

---
+++ js/script.js
@@ -0,0 +1,14 @@
+
+/* ----- loading animation ----- 
+$(window).load(function() {
+	$('#loading').hide();
+});*/
+
+/* ----- Home page hero animation ----- 
+$(document).ready(function(){
+
+	var scene = document.getElementById('scene');
+	var parallaxInstance = new Parallax(scene);
+	
+});
+*/
